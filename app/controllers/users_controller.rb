f2fff94a91b996bbe2ffd984cbd36d53fd6602d0
haileybongo <haileyebongo@gmail.com>
2020-05-09 11:42:59 -0700

    updating home view & adding log oout button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   get "/home" do 
-    binding.pry
     if logged_in?
       @user = current_user 
       erb :"/users/home.html"
@@ -70,7 +69,6 @@ class UsersController < ApplicationController
 
   post '/login' do
     user = User.find_by(:username => params[:username])
-    
     if user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/home"
