a155313e0c24c3950ca3c8d6293f6762118ff481
haileybongo <haileyebongo@gmail.com>
2020-05-22 12:17:58 -0700

    updating session key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 require './config/environment'
 
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, "cb386f4f44f3fc2b7cea2901023d897016000cc7ccb497ecb2ed9dec208d12c8a9786e36eb2fe67a070168f2224a5aad8141b4a9f1b60c73c2301fcbd67a9b34"
   end
 
   get "/" do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :plants
-    has_many :waters, through :plants
     validates :email, uniqueness: true
     validates :username, uniqueness: true
 end
