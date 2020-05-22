cea8e7d901537f77b11b69adcce07653b884db14
haileybongo <haileyebongo@gmail.com>
2020-05-21 21:12:11 -0700

    Updating AR Associations

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,4 @@
 class Plant < ActiveRecord::Base
     belongs_to :user
-    has_many :water
+    has_one :water
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :plants
+    has_many :waters, through :plants
     validates :email, uniqueness: true
     validates :username, uniqueness: true
 end
